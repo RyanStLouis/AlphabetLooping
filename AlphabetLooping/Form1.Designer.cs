bba9cb309a0367789228caf2896eaa5c3d45774f
RyanStLouis <ryan.stlouis@ocsbstudent.ca>
2015-10-20 14:14:01 -0400

    alphabet looping

---
+++ AlphabetLooping/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace AlphabetLooping
+{
+    partial class frmAlphabetLooping
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstBox
+            // 
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.Location = new System.Drawing.Point(83, 65);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(162, 238);
+            this.lstBox.TabIndex = 0;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(125, 24);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 1;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // frmAlphabetLooping
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 370);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.lstBox);
+            this.Name = "frmAlphabetLooping";
+            this.Text = "Alphabet Looping";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstBox;
+        private System.Windows.Forms.Button btnEnter;
+    }
+}
+
---
+++ AlphabetLooping/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlphabetLooping
+{
+    public partial class frmAlphabetLooping : Form
+    {
+        public frmAlphabetLooping()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            // variables
+            int firstNumber;
+            int secondNumber;
+            // clear listbox
+            this.lstBox.Items.Clear();
+
+            for (firstNumber = 65; firstNumber <= 90; firstNumber++)
+            {
+
+                for (secondNumber = 97; secondNumber <= 122; secondNumber++)
+                {
+
+                    this.lstBox.Items.Add(Char.ConvertFromUtf32(firstNumber) + " -> " + Char.ConvertFromUtf32(secondNumber));
+                }
+            }
+        }
+    }
+}
